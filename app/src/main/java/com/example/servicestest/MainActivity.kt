64747620dfb25b37d98b96fbab6b2927e91cf429
aleksvvv <adm-shop-001@yandex.ru>
2022-12-09 21:50:16 +0300

    step 5

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -1,14 +1,80 @@
 package com.example.servicestest
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
+import android.content.ComponentName
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.*
+import androidx.core.content.getSystemService
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import com.example.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var page = 0
+    private var id = 0
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.simpleService.setOnClickListener {
+            stopService(MyForegroundService.newIntent(this))
+            startService(MyService.newInstance(this, 24))
+
+        }
+
+        binding.foregroundService.setOnClickListener {
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this)
+            )
+//            startService(MyForegroundService.newIntent(this))
+//            ContextCompat.startForegroundService(this, MyService2.newInstance(this))
+
+        }
+        binding.intentService.setOnClickListener {
+            startService(MyIntentService.newIntent(this))
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(111, componentName)
+//                .setExtras(MyJobService.newBundle(page++))
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+//            jobScheduler.schedule(jobInfo)
+            val intent = MyJobService.newIntent(page++)
+            jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+        }
+        binding.jobIntentService.setOnClickListener {
+            MyJobIntentService.enqueue(this, page++)
+        }
+        binding.workManager.setOnClickListener {
+            val workManager = WorkManager.getInstance(applicationContext)
+            workManager.enqueueUniqueWork(
+                NAME_WORK,
+                ExistingWorkPolicy.APPEND,
+                MyWorker.makeRequest(page++)
+            )
+        }
+    }
+
+    companion object {
+
+        const val NAME_WORK = "my_work"
+        const val CHANNEL_NAME = "channel_name"
     }
 }
